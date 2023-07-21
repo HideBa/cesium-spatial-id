046f31145e1059f25c49ec4530df64e097eaa818
HideBa <baba.papa1120.ba@gmail.com>
2023-07-21 15:40:11 +0900

    init

---
+++ examples/src/index.ts
@@ -0,0 +1,28 @@
+import "./style.css";
+import { Viewer, Cartesian3, Color } from "cesium";
+
+declare global {
+  interface Window {
+    CESIUM_BASE_URL?: string;
+  }
+}
+
+const viewer = new Viewer("cesium");
+viewer.entities.add({
+  name: "Example1",
+  position: Cartesian3.fromDegrees(-100.0, 40.0, 100000.0),
+  box: {
+    dimensions: new Cartesian3(500000.0, 500000.0, 500000.0),
+    material: Color.AQUA,
+    outline: true,
+    outlineColor: Color.AQUA,
+  },
+});
+viewer.entities.add({
+  name: "Example2",
+  position: Cartesian3.fromDegrees(-120.0, 30.0, 1000.0),
+  ellipsoid: {
+    radii: new Cartesian3(500000.0, 500000.0, 500000.0),
+    material: Color.WHITE,
+  },
+});
